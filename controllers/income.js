daed04e5ea4d134334784b5aadc80e7cae17a8ac
Tranquil023 <prashantkashyap2707@gmail.com>
2025-10-23 14:06:11 +0530

    done changes

--- controllers/income.js
+++ controllers/income.js
@@ -47,9 +47,8 @@ const rechargeWallet = async (req, res) => {
 
         // 3️⃣ Return success response
         return res.status(200).json({
-            message: "Recharge request added to recharge and income_records tables",
+            message: "Recharge request added to recharge table",
             rechargeData,
-            incomeData,
         });
     } catch (error) {
         return res.status(500).json({ error: error.message });
